175d255af4a1c67300567b0dd551c8ff2b68d45b
SShowbiz <hwkim408@gmail.com>
2023-02-06 11:58:23 +0000

    fix: change hard-coded episode number to crawl

--- crawl.py
+++ crawl.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     if not os.path.exists(f"{args.name_en}/{args.no}/"):
         os.makedirs(f"{args.name_en}/{args.no}/")
     crawler = NaverWebtoonCrawl(args.name_en)
-    crawler.save_episode_images(no=228)
+    crawler.save_episode_images(no=args.no)
