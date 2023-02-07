412528f0ef3e9f9b21c76935abee5f6d6f50cffc
SShowbiz <hwkim408@gmail.com>
2023-02-07 01:50:47 +0000

    feat: add saving all episode command option and remove unnecessary arguments

--- crawl.py
+++ crawl.py
@@ -3,12 +3,15 @@ from nwc import NaverWebtoonCrawl
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name_en", default="TrueBeauty")  # extra value
+parser.add_argument("--name_en", default="TrueBeauty")
 parser.add_argument("--no", default="1")
-parser.add_argument("--save_dir", default="")
+parser.add_argument("--all", action="store_true")
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
     crawler = NaverWebtoonCrawl(args.name_en)
+    if args.all:
+        crawler.save_all_images()
+    else:
     crawler.save_episode_images(no=args.no)
